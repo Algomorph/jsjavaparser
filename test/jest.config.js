fefd82b808f80911ae1af191443eac0b9619b497
Gregory Kramida <algomorph@gmail.com>
2021-04-01 15:25:59 -0400

    Upgrade coverage system to Jest (completely remove qUnit)

--- test/jest.config.js
+++
@@ -1,8 +0,0 @@
-// jest.config.js
-// Sync object
-module.exports = {
-    verbose: true,
-    "collectCoverage": true,
-    "coverageReporters": ["json", "html"]
-    // modulePathIgnorePatterns: ["ParenthesizedExpression"]
-};
